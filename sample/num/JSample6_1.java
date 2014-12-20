768ba93da7f677c9a141f82141672dc004431ed7
wenbo <wen_bo@kembo-net.com>
2014-12-20 12:11:35 +0800

    sample/num/JSample6_1.java: 8進数、16進数で整数を表現する,  sample/num/JSample7_1.java: 指数形式で浮動小数点数を表現する

---
+++ sample/num/JSample6_1.java
@@ -0,0 +1,6 @@
+class JSample6_1{
+  public static void main(String args[]){
+    System.out.println(012);
+    System.out.println(0x5F);
+  }
+}
---
+++ sample/num/JSample7_1.java
@@ -0,0 +1,6 @@
+class JSample7_1{
+  public static void main(String args[]){
+    System.out.println(3.2e2);
+    System.out.println(2.4e-3);
+  }
+}
